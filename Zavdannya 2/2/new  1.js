ecd1b0cd65513d71cc7bcec5bb683e4feec70a9e
YarCher <yarcher@meta.ua>
2017-02-03 22:07:07 +0200

    Zavdannya 2

---
+++ Zavdannya 2/2/new  1.js
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Условная конструкция if</title>
+
+    <script type="text/javascript">
+
+        var a = prompt ;
+
+        if (a == "22") {
+            document.write ("Введенное значение верно. <br/>") ;
+        }
+
+        document.write ("Эта строка выполнится в любом случае") ;
+
+    </script>
+
+</head>
+<body>
+</body>
+</html>
